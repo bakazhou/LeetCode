954a9496491645e8a685c4d600fd772b566e0a17
ltz1fo1 <706515174@qq.com>
2022-04-26 13:37:59 +0800

    completed LeetCode 386,396,398

--- src/DailyCode/topic386/Solution.java
+++ src/DailyCode/topic386/Solution.java
@@ -14,22 +14,19 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
-        Collections.sort(list, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                final String num1 = o1.toString();
-                final String num2 = o2.toString();
-                int length = Math.min(num1.length(),num2.length());
-                for (int i = 0; i < length; i++) {
-                    if (num2.charAt(i) > num1.charAt(i)){
-                        return 1;
-                    }
-                    if (num2.charAt(i) < num1.charAt(i)){
-                        return -1;
-                    }
+        Collections.sort(list, (o1, o2) -> {
+            final String num1 = o1.toString();
+            final String num2 = o2.toString();
+            int length = Math.min(num1.length(),num2.length());
+            for (int i = 0; i < length; i++) {
+                if (num2.charAt(i) > num1.charAt(i)){
+                    return 1;
                 }
-                return num1.length()>num2.length()?-1:1;
+                if (num2.charAt(i) < num1.charAt(i)){
+                    return -1;
+                }
             }
+            return num1.length()>num2.length()?-1:1;
         });
         return list;
     }
